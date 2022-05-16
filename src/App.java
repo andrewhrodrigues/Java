d3622a35c43d6d2c89ac6af54048ceef25fce1f3
Andrew Rodrigues <andrewhrodrigues@gmail.com>
2022-05-16 08:28:03 -0300

    Atualização

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo");
+        String nome;
+        int idade;
+        double valor;
+        boolean bool;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome");
+        nome = sc.nextLine();
+        
+        System.out.println("Digite sua idade");
+        idade = sc.nextInt();
+        
+        System.out.println("Digite seu salário");
+        valor = sc.nextDouble();
+        
+        System.out.println(nome + " " + idade + " " + valor);
+        
+
+
     }
 }
+
+
+
+
+
+    
+ 
